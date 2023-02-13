1b3131fd9e814f557a310b48315058834b6d29c5
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2023-02-13 20:07:51 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(leftsensor)
+    drive = 1
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(rightsensor)
+    drive = 0
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showNumber(sensordiff)
+	
 })
 let sensordiff = 0
-let rightsensor = 0
 let leftsensor = 0
+let rightsensor = 0
+let drive = 0
 Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
 basic.forever(function () {
     rightsensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
@@ -24,8 +25,10 @@ basic.forever(function () {
                 . . . # .
                 . . # . .
                 `)
-            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            if (1 == drive) {
+                Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+                Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            }
         } else {
             basic.showLeds(`
                 . . # . .
@@ -34,8 +37,10 @@ basic.forever(function () {
                 . # . . .
                 . . # . .
                 `)
-            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            if (1 == drive) {
+                Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+                Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            }
         }
     } else {
         basic.showLeds(`
@@ -45,6 +50,8 @@ basic.forever(function () {
             . . # . .
             . . # . .
             `)
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
+        if (1 == drive) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
+        }
     }
 })
