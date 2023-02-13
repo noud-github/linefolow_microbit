622d7f4061e861779fcb3a328306e17405aecd23
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2023-02-13 19:46:55 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
 basic.forever(function () {
     rightsensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
     leftsensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
-    sensordiff = Math.abs(leftsensor) - Math.abs(rightsensor)
+    sensordiff = Math.abs(leftsensor) - rightsensor
     if (sensordiff > 10) {
         if (rightsensor < leftsensor) {
             basic.showLeds(`
