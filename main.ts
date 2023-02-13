4f15dd95dc4dffaf8ed07ea04988624d3e1f2c39
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2023-02-13 19:54:07 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 43)
+    basic.showNumber(leftsensor)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left))
+    basic.showNumber(rightsensor)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Kitronik_Move_Motor.stop()
+    basic.showNumber(sensordiff)
 })
 let sensordiff = 0
-let leftsensor = 0
 let rightsensor = 0
+let leftsensor = 0
 Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
 basic.forever(function () {
     rightsensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
     leftsensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
-    sensordiff = Math.abs(leftsensor) - rightsensor
+    sensordiff = Math.abs(leftsensor - rightsensor)
     if (sensordiff > 10) {
         if (rightsensor < leftsensor) {
             basic.showLeds(`
