f4d27b79765c78f69c8acb094290317ee314fd61
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2023-02-13 20:14:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,44 +12,27 @@ let leftsensor = 0
 let rightsensor = 0
 let drive = 0
 Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+radio.setGroup(1)
 basic.forever(function () {
     rightsensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
     leftsensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
     sensordiff = Math.abs(leftsensor - rightsensor)
     if (sensordiff > 10) {
         if (rightsensor > leftsensor) {
-            basic.showLeds(`
-                . . # . .
-                . . . # .
-                # # # . #
-                . . . # .
-                . . # . .
-                `)
+            radio.sendString("left")
             if (1 == drive) {
                 Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
                 Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
             }
         } else {
-            basic.showLeds(`
-                . . # . .
-                . # . . .
-                # . # # #
-                . # . . .
-                . . # . .
-                `)
+            radio.sendString("right")
             if (1 == drive) {
                 Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
                 Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 30)
             }
         }
+        radio.sendString("stright")
     } else {
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
         if (1 == drive) {
             Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
         } else {
